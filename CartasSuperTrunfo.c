12c794b8fc252070be3755084b58c737f1819f32
CharlesFeliz <202602107511@alunos.estacio.br>
2026-02-23 23:54:40 +0000

    Desafio com comparações

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,114 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+int populacao1,npt1,populacao2,npt2;
+float area1,pib1,area2,pib2, dp1,dp2,pibc1,pibc2;
+char estado1,estado2;
+char n_cidade1[50], cod_cidade1[50],n_cidade2[50], cod_cidade2[50];
 
-  // Área para entrada de dados
+printf("\n\nCarta 01: \n");
+printf("Digite a letra do estado:");
+scanf("%c", &estado1);
 
-  // Área para exibição dos dados da cidade
+printf("Digite o código da cidade:");
+scanf("%s", &cod_cidade1);
+
+printf("Digite o nome da cidade:");
+scanf("%s", &n_cidade1);
+
+printf("Digite o numero da população:");
+scanf("%d", &populacao1);
+
+printf("Digite a area da cidade:");
+scanf("%f", &area1);
+
+printf("Digite o PIB da cidade:");
+scanf("%f", &pib1);
+
+printf("Digite o número de pontos turísticos da cidade:");
+scanf("%d", &npt1);
+
+printf("\n\nCarta 02: \n");
+
+printf("Digite a letra do estado:");
+scanf("%c", &estado2);
+
+printf("Digite o código da cidade:");
+scanf("%s", &cod_cidade2);
+
+printf("Digite o nome da cidade:");
+scanf("%s", &n_cidade2);
+
+printf("Digite o numero da população:");
+scanf("%d", &populacao2);
+
+printf("Digite a area da cidade:");
+scanf("%f", &area2);
+
+printf("Digite o PIB da cidade:");
+scanf("%f", &pib2);
+
+printf("Digite o número de pontos turísticos da cidade:");
+scanf(" %d", &npt2);
+
+printf("\n\nCarta 01: \n");
+
+printf("O estado: %s\n", estado1);
+printf("O código da cidade: %s\n", cod_cidade1);
+printf("O nome da cidade é: %s\n", n_cidade1);
+printf("A população é: %d habitantes\n", populacao1);
+printf("A área é: %f km²\n", area1);
+printf("O PIB é: %f bilhões de reais\n", pib1);
+printf("O número de pontos turísticos: %d\n", npt1);
+dp1=populacao1/area1;
+pibc1=(pib1/populacao1)*1000;
+printf("A densidade populacional é: %.2f hab./km2\n", dp1);
+printf("O PIB per Capita é: %.4f milhões de reais\n", pibc1);
+
+printf("\n\nCarta 02: \n");
+
+printf("O estado é: %s\n", estado2);
+printf("O código da cidade: %s\n", cod_cidade2);
+printf("O nome da cidade é: %s\n", n_cidade2);
+printf("A população é: %d habitantes\n", populacao2);
+printf("A área é: %.2f km²\n", area2);
+printf("O PIB é: %.2f bilhões de reais\n", pib2);
+printf("O número de pontos turísticos é: %d\n", npt2);
+dp2=populacao2/area2;
+pibc2=(pib2/populacao2)*1000;
+printf("A densidade populacional é: %.2f hab./km2\n", dp2);
+printf("O PIB per Capita é: %.4f milhões de reais\n", pibc2);
+
+printf("\n\nComparando as Cartas \n");
+
+if (dp1<dp2)
+ printf ("A densidade populacional da carta 01 é a vencedora\n");
+else
+ printf ("A densidade populacional da carta 02 é a vencedora\n");
+
+if (populacao1>populacao2)
+  printf ("A população da carta 01 é a vencedora\n");
+else
+  printf ("A população da carta 02 é a vencedora\n");
+if (area1>area2)
+ printf ("A área da carta 01 é a vencedora\n");
+else
+ printf ("A área da carta 02 é a vencedora\n");
+ 
+if (pib1>pib2)
+printf ("O PIB da carta 01 é a vencedora\n");
+else
+printf ("O PIB da carta 02 é a vencedora\n");
+
+if (pibc1>pibc2)
+printf ("O PIB per capito da carta 01 é a vencedora\n");
+else
+printf ("O PIB per capito da carta 02 é a vencedora\n");
+
+if (npt1>npt2)
+printf ("A carta 01 é a vencedora, possui o maior numero de pontos turisticos\n");
+else
+printf ("A carta 02 é a vencedora, possui o maior numero de pontos turisticos\n");
 
 return 0;
-} 
+}
